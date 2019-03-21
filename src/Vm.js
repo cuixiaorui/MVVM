93d4f4451d5bc893cb4c9dd47248eb11da4652ab
崔效瑞 <cuixiaorui@100tal.com>
2019-03-21 11:13:59 +0800

    feat: 增加 vm.xx -> vm._data.xx 代理

--- src/Vm.js
+++ src/Vm.js
@@ -5,11 +5,14 @@ export default class Vm{
         this._config = config;
         this._data = config.data;
         this._el = config.$el
+        this._methods = config.methods;
         // 给 data 内的所有属性增加 getter 和 setter 
         this.observer(this._data)
         // 解析 dom 
         new Compiler(this._el,this); 
-         
+
+
+        this.registProxy(this._data);
     }
 
     observer(data){
@@ -39,5 +42,30 @@ export default class Vm{
 
     get data(){
         return this._data;
+    }
+
+    /**
+     * this._data 的代理
+     * this.xx ==> this._data.xx
+     * @param {} data 
+     */
+    registProxy(data){
+        if(!data || typeof data !== "object")return;
+        Object.keys(data).forEach((key)=>{
+            this.registProxy(data[key]);
+            Object.defineProperty(this,key,{
+                get(){
+                    return data[key]         
+                },
+                set(val){
+                    if(val !== this._data[key])
+                    {
+                        data[key] = val
+                    }
+                }
+            })
+        })
+         
+
     }
 }
